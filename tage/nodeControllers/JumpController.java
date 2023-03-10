99607273a6423e004221939c1f7e882e5c81cb5d
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-03-10 14:11:02 -0800

    Added an empty jumpcontroller

---
+++ tage/nodeControllers/JumpController.java
@@ -0,0 +1,20 @@
+package tage.nodeControllers;
+
+import tage.*;
+import org.joml.*;
+
+public class JumpController extends NodeController
+{
+    private Engine engine;
+
+    public JumpController(Engine e)
+    { 
+        super();
+        engine = e;
+    }
+
+    public void apply(GameObject go)
+    { 
+        
+    }
+}
